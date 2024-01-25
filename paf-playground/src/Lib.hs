7e4bdf2ef04256299a94ee7845cbea809ff51a5d
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-01-25 20:59:03 +0100

    TME 1 rendu initial.

--- paf-playground/src/Lib.hs
+++ paf-playground/src/Lib.hs
@@ -1,6 +1,20 @@
 module Lib
     ( someFunc
+    , maxInt
+    , fibonacci
     ) where
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+maxInt :: Integer -> Integer -> Integer
+maxInt n m 
+    | n<m = m
+    | otherwise = n
+
+fibonacci :: Integer -> Integer
+fibonacci n
+    | n==0 = 0
+    | n==1 = 1
+    | otherwise = fibonacci(n-1) + fibonacci(n-2)
